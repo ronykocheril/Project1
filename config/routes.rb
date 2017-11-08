b444fb0b5b97364a0751f5f5670df303158c315c
Anmol Sandhu <anmolsandhu3096@gmail.com>
2017-11-08 03:01:10 -0600

    Added product controller to the rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'product/index'
+
+  root to: 'home#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
