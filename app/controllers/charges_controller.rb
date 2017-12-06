d7311fa545f7e0d1787adcc31c3d2054da408635
Rony <ronykocheril@gmail.com>
2017-12-06 19:20:13 +0000

    Successfull checkout is added to lineitems

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,11 +6,33 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    obj1 = Order.create(:status => "Paid", :pst => 8.0, :gst => 9.0, :hst => 5.6, :user_id => 1, :name => "name")
+    @cart_session = session[:c]
+    @current_user = current_user
+
+    @total = params[:total]
+
+    @province_details = Province.where("id LIKE ?", @current_user.id).first
+
+
+    @calc_gst = @total.to_f * @province_details.gst.to_f
+    @calc_pst = @total.to_f * @province_details.pst.to_f
+    @calc_hst = @total.to_f * @province_details.hst.to_f
+
+    obj1 = Order.create(:status => "Paid", :pst => @calc_pst, :gst => @calc_gst, :hst => @calc_hst, :user_id => @current_user.id, :name => "Order_#{@current_user.id}" )
     obj1.save
 
-    obj2 = Lineitem.create(:price => 20.0, :quantity => 2, :product_id => 3, :order_id => obj1.id)
-    obj2.save
+
+
+    @cart_session.each do |each_array|
+      each_array_id = each_array['id'].to_i
+      each_quantity = each_array['quantity'].to_f
+      product = Product.where('id = ?', each_array_id ).first
+      @price = each_quantity* product.price
+
+      obj2 = Lineitem.create(:price => @price, :quantity => each_quantity , :product_id => product.id, :order_id => obj1.id)
+      obj2.save
+    end
+
 
     @amount = session[:total]
 
