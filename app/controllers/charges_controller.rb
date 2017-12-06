2533ba97b48e27f0dccd7bdc039fcb13fa84fe6c
Rony <ronykocheril@gmail.com>
2017-12-06 18:58:58 +0000

    Modified checkout

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -22,6 +22,11 @@ class ChargesController < ApplicationController
     rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
+
+
+    obj1 = Order.create(:status => "Paid", :pst => 8.0, :gst => 9.0, :hst => 5.6, :user_id => 1, :name => "name")
+    obj1.save
+
   end
 
   def update_order
