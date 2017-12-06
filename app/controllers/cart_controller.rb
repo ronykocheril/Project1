4766f0412f3b72cd6790739a1ba2e52f380bec23
Rony <ronykocheril@gmail.com>
2017-12-06 18:51:30 +0000

    Modified cart page

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -39,7 +39,19 @@ class CartController < ApplicationController
   end
 
   def checkout
+    @cart_session = session[:c]
+    @current_user = current_user
+
     @total = params[:total]
+
+    @province_details = Province.where("id LIKE ?", @current_user.id).first
+
+    @calc_gst = @total.to_f * @province_details.gst.to_f
+    @calc_pst = @total.to_f * @province_details.pst.to_f
+
+
+
+
 
   end
 
